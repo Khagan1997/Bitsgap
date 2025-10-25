54c39dc1c3117c36d5eaf440cf6b376f0a53430a
Khagan <khaqan.rustamov@gmail.com>
2025-10-25 15:33:53 +0400

    init project

---
+++ playwright.config.js
@@ -0,0 +1,25 @@
+import { defineConfig, devices } from "@playwright/test";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export default defineConfig({
+  testDir: "./src/tests",
+  timeout: 120000,
+  fullyParallel: true,
+  retries: 1,
+  reporter: [["html", { outputFolder: "playwright-report" }]],
+  use: {
+    baseURL: process.env.APP_URL,
+    headless: true,
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    video: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
